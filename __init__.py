33adc71058be359420262f1b3bbf45ca62310545
Гамид Агамов <gamidagamov@MacBook-Air-Gamid.local>
2025-10-31 00:54:35 +0300

    Update reristration

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from core.plugins.global_registry import plugin_registry
 from .plugin import Plugin
 
-# Регистрируем плагин в глобальном реестре
-plugin_registry.register("template", lambda config, db: Plugin(config, db))
+# Автоматическая регистрация - имя определяется из директории
+plugin_registry.register(lambda config, db: Plugin(config, db))
